5ae1352234d398fab4bc55ef33a3d0f6160c72ca
aryana3ri <aryana3rihosein246810@gmail.com>
2026-07-04 00:26:32 +0330

    Set Dunder Method
    to Show Object Im set Majic Method to Notes Class

--- notes/models.py
+++ notes/models.py
@@ -8,3 +8,6 @@ class Notes(models.Model):
     create_time = models.DateTimeField(auto_now_add="True") 
     update_time = DateTimeField(auto_now="True") 
     
+    def __str__(self): 
+    return f"{self.name}-{self.create_time}"
+    
